8ab0e8256105c2915f2d8f10608a1d143af545dd
Jeffrey Ruane <ruane.jb@gmail.com>
2016-04-21 14:53:03 -0600

    Finish PHP Raindrops

--- php/raindrops/raindrops.php
+++ php/raindrops/raindrops.php
@@ -2,5 +2,21 @@
 
 function raindrops($num)
 {
-  return "{$num}";
+  $result = "";
+
+  $raindrops_array = [
+    3 => "Pling",
+    5 => "Plang",
+    7 => "Plong"
+  ];
+
+  foreach ($raindrops_array as $key => $value) {
+    if ($num%$key == 0)
+      $result .= $value;
+  }
+
+  if (strlen($result) == 0)
+    return "$num";
+  else
+    return $result;
 }
