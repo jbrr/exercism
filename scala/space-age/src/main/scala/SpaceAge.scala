7c41f306ebf0ebf0bfbd9f30c1f1a069d62c3110
Jeffrey Ruane <ruane.jb@gmail.com>
2020-07-19 22:10:55 -0600

    starting scala

---
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -0,0 +1,14 @@
+object SpaceAge{
+  val EARTH_ORBIT_IN_SECONDS = 31557600
+
+  val onMercury: Double => Double = (age: Double) => toYears(age, 0.2408467)
+  val onVenus: Double => Double = (age: Double) => toYears(age, 0.61519726)
+  val onEarth: Double => Double = (age: Double) => toYears(age, 1.0)
+  val onMars: Double => Double = (age: Double) => toYears(age, 1.8808158)
+  val onJupiter: Double => Double = (age: Double) => toYears(age, 11.862615)
+  val onSaturn: Double => Double = (age: Double) => toYears(age, 29.447498)
+  val onUranus: Double => Double = (age: Double) => toYears(age, 84.016846)
+  val onNeptune: Double => Double = (age: Double) => toYears(age, 164.79132)
+
+  private def toYears(age: Double, orbitalPeriodInEarthYears: Double): Double = age / (EARTH_ORBIT_IN_SECONDS * orbitalPeriodInEarthYears)
+}
