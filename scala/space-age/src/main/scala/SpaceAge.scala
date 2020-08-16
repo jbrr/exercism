451c1595d10bd2edc5f9dba5766e4440d2add0dc
Jeffrey Ruane <ruane.jb@gmail.com>
2020-08-15 22:53:10 -0600

    More exercises

--- scala/space-age/src/main/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -1,5 +1,5 @@
 object SpaceAge{
-  val EARTH_ORBIT_IN_SECONDS = 31557600
+  val EarthOrbitInSeconds = 31557600
 
   val onMercury: Double => Double = (age: Double) => toYears(age, 0.2408467)
   val onVenus: Double => Double = (age: Double) => toYears(age, 0.61519726)
@@ -10,5 +10,5 @@ object SpaceAge{
   val onUranus: Double => Double = (age: Double) => toYears(age, 84.016846)
   val onNeptune: Double => Double = (age: Double) => toYears(age, 164.79132)
 
-  private def toYears(age: Double, orbitalPeriodInEarthYears: Double): Double = age / (EARTH_ORBIT_IN_SECONDS * orbitalPeriodInEarthYears)
+  private def toYears(age: Double, orbitalPeriodInEarthYears: Double): Double = age / (EarthOrbitInSeconds * orbitalPeriodInEarthYears)
 }
---
+++ scala/spiral-matrix/src/main/scala/SpiralMatrix.scala
@@ -0,0 +1,5 @@
+object SpiralMatrix {
+  def spiralMatrix(size: Int): List[Any] = {
+
+  }
+}
